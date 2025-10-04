0d6840431962b80371562f33ec90d50d82559379
Yinchu <yinchux@andrew.cmu.edu>
2025-10-03 23:26:33 -0400

    finish initializer

--- python/needle/init/init_initializers.py
+++ python/needle/init/init_initializers.py
@@ -5,19 +5,23 @@ from typing import Any
 
 def xavier_uniform(fan_in: int, fan_out: int, gain: float = 1.0, **kwargs: Any) -> "Tensor":
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    a = gain * math.sqrt(6.0 / (fan_in + fan_out))
+    return rand(fan_in, fan_out, low=-a, high=a, **kwargs)
     ### END YOUR SOLUTION
 
 
 def xavier_normal(fan_in: int, fan_out: int, gain: float = 1.0, **kwargs: Any) -> "Tensor":
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    std = gain * math.sqrt(2.0 / (fan_in + fan_out))
+    return randn(fan_in, fan_out, mean=0.0, std=std, **kwargs)
     ### END YOUR SOLUTION
 
 def kaiming_uniform(fan_in: int, fan_out: int, nonlinearity: str = "relu", **kwargs: Any) -> "Tensor":
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2.0)
+    bound = gain * math.sqrt(3.0 / fan_in)
+    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
     ### END YOUR SOLUTION
 
 
@@ -25,5 +29,7 @@ def kaiming_uniform(fan_in: int, fan_out: int, nonlinearity: str = "relu", **kwa
 def kaiming_normal(fan_in: int, fan_out: int, nonlinearity: str = "relu", **kwargs: Any) -> "Tensor":
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2.0)
+    std = gain / math.sqrt(fan_in)
+    return randn(fan_in, fan_out, mean=0.0, std=std, **kwargs)
     ### END YOUR SOLUTION
